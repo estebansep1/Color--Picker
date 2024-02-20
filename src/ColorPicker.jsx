cd126db853adc129cb145875b4168b253fa13b39
Esteban Sepulveda <tebansd@icloud.com>
2024-02-20 15:27:50 -0800

    implemented full color picker feature with basic styling

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from "react";
+
 export default function ColorPicker() {
-    return(<></>)
+  const [color, setColor] = useState (() => {
+    return localStorage.getItem("color") || "#FFFFFF";
+  })
+
+  useEffect (() => {
+    localStorage.setItem("color", color);
+  }, [color])
+
+  function handleColorChange(event) {
+    setColor(event.target.value);
+  }
+
+  
+
+  return (
+    <div className="color--picker--container">
+      <h1>Color Picker</h1>
+      <div className="color--display" style={{backgroundColor: color}}>
+        <p>Selected Color: {color}</p>
+      </div>
+      <label>Select a Color:</label>
+      <input type="color" value={color} onChange={handleColorChange} />
+    </div>
+  )
 }
